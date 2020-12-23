02007af84b6ffca7a4dfe7eec8e40fd30212ed4a
ttdanh282 <ttdanh282@gmail.com>
2020-12-23 16:16:08 +0700

    add awesome font

---
+++ public/js/myscript.js
@@ -0,0 +1,23 @@
+Chart.defaults.global.defaultFontColor = 'white';
+let ctxLine,
+    ctxBar,
+    ctxPie,
+    optionsLine,
+    optionsBar,
+    optionsPie,
+    configLine,
+    configBar,
+    configPie,
+    lineChart;
+barChart, pieChart;
+// DOM is ready
+$(function () {
+    drawLineChart(); // Line Chart
+    drawBarChart(); // Bar Chart
+    drawPieChart(); // Pie Chart
+
+    $(window).resize(function () {
+        updateLineChart();
+        updateBarChart();                
+    });
+})
