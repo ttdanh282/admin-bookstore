224f4d03ea95216189eb416c4c6bfd1201826362
ttdanh282 <ttdanh282@gmail.com>
2021-01-03 22:27:32 +0700

    add detail

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,6 +1,8 @@
 const bookService = require('../models/services/bookService');
 exports.allbook = async (req, res, next) => {
     let books =  await bookService.list();
+    console.log(books);
+    console.log("Book dau tien"+ books[0].category[0].name);
     res.render('books/book',{books});
 }
 
--- models/services/bookService.js
+++ models/services/bookService.js
@@ -1,16 +1,33 @@
 const bookModel = require('../bookModel');
 const { ObjectId} = require('mongodb');
+// const categoryModel = require('../categoyModel');
+
+// exports.list = async () => {
+//      return await bookModel.find(function(err, resp){
+//         if (err) return handleError(err);
+//         return resp;
+//     }).lean();
+// };
 
 exports.list = async () => {
-     return await bookModel.find(function(err, resp){
-        if (err) return handleError(err);
-        return resp;
-    }).lean();
+    return await bookModel
+        .aggregate([{
+            $lookup:
+            {
+                from: "categories",
+                localField: "category",
+                foreignField: "_id",
+                as: "category"
+            }
+        }], function(err, resp){
+            if (err) return handleError(err);
+            return resp;
+        })
 };
 
 exports.detail = async (id) => {
     return await bookModel.findOne({_id: ObjectId(id)}, function(err, resp){
         if (err) return handleError(err);
-        return resp;
+        return resp; 
     }).lean();
 }
